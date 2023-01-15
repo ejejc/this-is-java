aa64cdd44d2b98e4b1427d1d8ba7ba1daed69137
eunjis <ejejc@naver.com>
2023-01-15 22:12:12 +0900

    익명 객체-익명 자식 객체 공부.

---
+++ src/main/java/com/example/thisisjava/nested_class_9/Tire.java
@@ -0,0 +1,7 @@
+package com.example.thisisjava.nested_class_9;
+
+public class Tire {
+    public void roll() {
+        System.out.println("일반 타이어가 굴러갑니다.");
+    }
+}
--- src/test/java/com/example/thisisjava/nested_class_9/nestedClassTest.java
+++ src/test/java/com/example/thisisjava/nested_class_9/nestedClassTest.java
@@ -65,4 +65,24 @@ class nestedClassTest {
         btnCancel.setClickListener(new CancelListener());
         btnCancel.click();
     }
+
+    @Test
+    public void 익명_자식_객체_테스트() {
+        // Car 객체 생성
+        Car car = new Car();
+
+        // 익명 자식 객체가 대입된 필드 사용
+        car.run1();
+
+        // 익명 자식 객체가 대입된 로컬변수 사용
+        car.run2();
+
+        // 익명 자식 객체가 대입된 매개변수 사용
+        car.run3(new Tire() {
+            @Override
+            public void roll() {
+                System.out.println("익명 자식 Tire 객체 3이 굴러갑니다.");
+            }
+        });
+    }
 }
